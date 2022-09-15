9e271e93aedeb731d4d253cc056f8c26cffe7b74
Le127 <nereocarbonara@gmail.com>
2022-09-15 17:35:03 -0300

    Fin sección 17 - BlocGps

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_app/screens/map_screen.dart';
+import 'package:maps_app/screens/screens.dart';
+
+import '../blocs/blocs.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<GpsBloc, GpsState>(
+        builder: (context, state) {
+          return (state.isAllGranted)
+              ? const MapScreen()
+              : const GpsAcessScreen();
+        },
+      ),
+    );
+  }
+}
